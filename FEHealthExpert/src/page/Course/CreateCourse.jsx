88ace369826e26182d594a16210331b4de80ad64
quanphamfpt <pquan3563@gmail.com>
2024-04-02 02:07:43 +0700

    Display Course, Course Detail, Bmi Propose

---
+++ FEHealthExpert/src/page/Course/CreateCourse.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { useState } from "react";
+
+const CreateCourse = () => {
+    const [course, setCourse] = useState({
+        courseId: '',
+        courseName: '',
+        price: '',
+        rating: '',
+        description: '',
+        studentNumber: '',
+        certificate: '',
+        createBy: '',
+        dateUpdate: new Date().toISOString(),
+        language: '',
+        bmiMin: '',
+        bmiMax: '',
+        typeId: 0
+    });
+
+    const [existingDataError, setExistingDataError] = useState('');
+
+    const handleChange = async (e) => {
+        const { name, value } = e.target;
+        setCourse({ ...course, [name]: value });
+
+        // Check existing courseId
+        if (name === 'courseId') {
+            try {
+                const response = await axios.get(`http://20.2.73.15:8173/api/Course/${value}`);
+                if (response.data) {
+                    // Course ID already exists
+                    setExistingDataError('Course ID already exists!');
+                }
+            } catch (error) {
+                console.log('Error checking Course ID:', error);
+                setExistingDataError('This Course ID can be used to create.');
+            }
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('http://20.2.73.15:8173/api/Course', course);
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Create Course</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="courseId" value={course.courseId} onChange={handleChange} placeholder="Course ID" />
+                {existingDataError && <p style={{ color: 'red' }}>{existingDataError}</p>}<br></br>
+                <input type="text" name="courseName" value={course.courseName} onChange={handleChange} placeholder="Course Name" /><br></br>
+                <input type="number" name="price" value={course.price} onChange={handleChange} placeholder="Price" /><br></br>
+                <input type="number" name="rating" value={course.rating} onChange={handleChange} placeholder="Rating" /><br></br>
+                <textarea name="description" value={course.description} onChange={handleChange} placeholder="Description" /><br></br>
+                <input type="number" name="studentNumber" value={course.studentNumber} onChange={handleChange} placeholder="Student Number" /><br></br>
+                <input type="text" name="certificate" value={course.certificate} onChange={handleChange} placeholder="Certificate" /><br></br>
+                <input type="text" name="createBy" value={course.createBy} onChange={handleChange} placeholder="Created By" /><br></br>
+                <input type="date" name="dateUpdate" value={course.dateUpdate} onChange={handleChange} placeholder="Date Update" /><br></br>
+                <input type="text" name="language" value={course.language} onChange={handleChange} placeholder="Language" /><br></br>
+                <input type="number" name="bmiMin" value={course.bmiMin} onChange={handleChange} placeholder="BMI Min" /><br></br>
+                <input type="number" name="bmiMax" value={course.bmiMax} onChange={handleChange} placeholder="BMI Max" /><br></br>
+                <input type="number" name="typeId" value={course.typeId} onChange={handleChange} placeholder="Type Id" /><br></br>
+                <button type="submit">Create</button>
+            </form>
+        </div>
+    );
+
+}
+
+export default CreateCourse;
--- FEHealthExpert/src/page/User/edit_profile.jsx
+++ FEHealthExpert/src/page/User/edit_profile.jsx
@@ -2,10 +2,32 @@ import React, { useState } from "react";
 import splash from '../../img/bg.png';
 import pfp from '../../img/pfp.png';
 import cover from '../../img/course_cover.png';
+import { Form, Input, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import Button from "../../components/button";
+import { DatePicker, Space } from 'antd';
+import { Select } from 'antd';
+
+import { Link, useNavigate } from 'react-router-dom'
 
 function EditProfile() {
   const [pic, setFileP] = useState();
   const [banner, setFileB] = useState();
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [fullName, setFullname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [birthDate, setBirthDate] = useState(null);
+  const [gender, setGender] = useState(null);
+
+  const handleDatePickerChange = (selectedDate) => {
+    setBirthDate(selectedDate);
+  };
+  const handleSelectChange = (selectedOption) => {
+    setGender(selectedOption.value);
+  };
 
   function changePfp(e) {
     console.log(e.target.files);
@@ -24,127 +46,180 @@ function EditProfile() {
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       {/* Main styles for this application*/}
       {/* idk */}
-      <div class="grid gap-4 ml-16 mr-16">
+      <div className="grid gap-4 ml-16 mr-16">
         {/* banner and pfp hijinx */}
-        <div class="">
-          <img src={banner ? banner : splash} alt="" class="relative object-cover w-full h-96 z-0" />
+        <div className="">
+          <img src={banner ? banner : splash} alt="" className="relative object-cover w-full h-96 z-0" />
         </div>
         {/* two columns */}
-        <div class="flex columns-2 gap-4">
+        <div className="flex columns-2 gap-4">
           {/* left column: idk pfp ?? */}
-          <div class="flex-auto w-2/5">
-            <div class="ml-5 mt-5 mb-5">
-              <img src={pic ? pic : pfp} alt="" class="object-scale-down w-48"/>
-              <label for="pfp">Change profile pic</label>
-              <br/>
-              <input type="file" id="pfp" style={{display:"none"}} onChange={changePfp} />
-              <label for="banner">Change banner</label>
+          <div className="flex-auto w-2/5">
+            <div className="ml-5 mt-5 mb-5">
+              <img src={pic ? pic : pfp} alt="" className="object-scale-down w-48" />
+              <label htmlFor="pfp">Change profile pic</label>
               <br />
-              <input type="file" id="banner" style={{display:"none"}} onChange={changeBanner} />
+              <input type="file" id="pfp" style={{ display: "none" }} onChange={changePfp} />
+              <label htmlFor="banner">Change banner</label>
+              <br />
+              <input type="file" id="banner" style={{ display: "none" }} onChange={changeBanner} />
               <br />
               <p className='text-xl font-bold'>my NAME???</p>
               <br />
-              <p class="">About page</p>
+              <p className="">About page</p>
             </div>
           </div>
           {/* right column: edit */}
-          <div class="flex-auto border rounded shadow-2xl w-3/5">
-            <div class="ml-5 mt-5 mb-5 mr-5">
+          <div className="flex-auto border rounded shadow-2xl w-3/5">
+            <div className="ml-5 mt-5 mb-5 mr-5">
               {/* ----- */}
-              <div className="w-36">
-                <label htmlFor="uname" className="text-left">
-                  Username:
-                </label>
+              <div className="mb-2">
+                <p>UserName</p>
               </div>
-              <div className="mt-2">
-                <input
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Username!",
+                  },
+                ]}
+              >
+                <Input
                   type="text"
-                  className="w-full border-2"
-                  id="uname"
-                  name="uname"
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Username"
+                  className="width:420px py-3 "
+                  onChange={(e) => setUserName(e.target.value)}
                 />
-                <br />
-                <br />
+              </Form.Item>
+              <div className="mb-2">
+                <p>Password</p>
               </div>
-              {/* ----- */}
-              <div className="w-36">
-                <label htmlFor="pwd" className="text-left">
-                  Password:
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Password!",
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
                   type="password"
-                  className="w-full border-2"
-                  id="pwd"
-                  name="pwd"
+                  placeholder="Password"
+                  className="width:420px py-3"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
-                <br />
-                <br />
+              </Form.Item>
+              <div className="mb-2">
+                <p>Confirm Password</p>
               </div>
-              {/* ----- */}
-              <div className="w-36">
-                <label htmlFor="rname" className="text-left">
-                  Real Name:
-                </label>
+              <Form.Item
+                name="confirm-password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Confirm Password!",
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="Password"
+                  className="width:420px py-3"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </Form.Item>
+              <div className="mb-2">
+                <p>Email</p>
               </div>
-              <div className="mt-2">
-                <input
+              <Form.Item
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Email!",
+                  },
+                ]}
+              >
+                {/* email input */}
+                <Input
                   type="text"
-                  className="w-full border-2"
-                  id="rname"
-                  name="rname"
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Email"
+                  className="width:420px py-3 "
+                  onChange={(e) => setEmail(e.target.value)}
                 />
-                <br />
-                <br />
+              </Form.Item>
+              <div className="mb-2">
+                <p>Full Name</p>
               </div>
-              {/* ----- */}
-              <div className="w-36">
-                <label htmlFor="email" className="text-left">
-                  Email:
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
+              <Form.Item
+                name="fullName"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Full Name!",
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
                   type="text"
-                  className="w-full border-2"
-                  id="email"
-                  name="email"
+                  placeholder="fullname"
+                  className="width:420px py-3"
+                  onChange={(e) => setFullname(e.target.value)}
                 />
-                <br />
-                <br />
+              </Form.Item>
+              <div className="mb-2">
+                <p>Phone Number</p>
               </div>
-              {/* ----- */}
-              <div className="w-36">
-                <label htmlFor="fname" className="text-left">
-                  Phone:
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
+              <Form.Item
+                name="phone"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your phome number!",
+                  },
+                ]}
+              >
+                <Input
                   type="text"
-                  className="w-full border-2"
-                  id="phone"
-                  name="phone"
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Phone Number"
+                  className="width:420px py-3"
+                  onChange={(e) => setPhone(e.target.value)}
                 />
-                <br />
-                <br />
-              </div>
-              {/* ----- */}
-              <div className="w-36">
-                <label htmlFor="fname" className="text-left">
-                  Birthday:
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  className="w-full border-2"
-                  id="bday"
-                  name="bday"
-                />
-                <br />
-                <br />
+              </Form.Item>
+              <div className="mb-2 flex">
+                <div className="mr-2">
+                  <p>Birthday</p>
+                  <Space direction="vertical" size={12}>
+                    <DatePicker
+                      selected={birthDate}
+                      onChange={handleDatePickerChange} />
+                  </Space>
+                </div>
+
+                <div>
+                  <p>Gender</p>
+                  <div className="flex">
+                    <Select
+                      defaultValue="Gender"
+                      onChange={handleSelectChange}
+                      options={[
+                        {
+                          options: [
+                            { label: 'Male', value: 'true' },
+                            { label: 'Female', value: 'false' },
+                          ]
+                        }
+                      ]} />
+                  </div>
+                </div>
               </div>
               {/* ----- */}
 

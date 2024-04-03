fe22c9f0be41ae0149b5307c7214c79628976f7d
unknown <dephongacc@gmail.com>
2024-04-03 20:11:21 +0700

    add button "update course" to profile

---
+++ fehealthexpert/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import Logo from "../img/logo.png";
+import { useNavigate } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Footer;
--- fehealthexpert/src/page/User/profile.jsx
+++ fehealthexpert/src/page/User/profile.jsx
@@ -112,38 +112,48 @@ function YourProfile() {
         {/* two columns */}
         <div className="flex columns-2 gap-4">
           {/* left column: about */}
-          <div className="flex-auto w-2/5">
+          <div className="flex-auto w-2/6">
             <div className="border rounded-t-lg shadow-2xl ">
-            <p className="ml-3 mt-3 font-bold">Giới thiệu</p>
-            <br />
+              <p className="ml-3 mt-3 font-bold">Giới thiệu</p>
+              <br />
 
-            <hr />
-            <p className="ml-3 text-center">Email: {email}</p>
-            <br />
-            <p className="ml-3 text-center">Số điện thoại: {phone}</p>
-            <br />
-            <p className="ml-3 text-center">Giới tính: {gender}</p>
-            <br />
-            <p className="ml-3 text-center">Ngày sinh: {formattedDate}</p>
-            <br />
-            <hr />
+              <hr />
+              <p className="ml-3 text-center">Email: {email}</p>
+              <br />
+              <p className="ml-3 text-center">Số điện thoại: {phone}</p>
+              <br />
+              <p className="ml-3 text-center">Giới tính: {gender}</p>
+              <br />
+              <p className="ml-3 text-center">Ngày sinh: {formattedDate}</p>
+              <br />
+              <hr />
             </div>
-          <a href="/editProfile">
-          <div class="border rounded-b-lg shadow-2xl bg-orange-400 hover:bg-orange-600 hover:underline hover:text-blue-400"  style={{ cursor: 'pointer' }}>
-              <p class="flex justify-center my-6 font-bold">CẬP NHẬT</p>
-          </div>
-          </a>
+            <a href="/editProfile">
+              <div class="border rounded-b-lg shadow-2xl bg-orange-400 hover:bg-orange-600 hover:underline hover:text-blue-400" style={{ cursor: 'pointer' }}>
+                <p class="flex justify-center my-6 font-bold">CẬP NHẬT</p>
+              </div>
+            </a>
           </div>
           {/* right column: joined courses */}
-          <div className="flex-auto border rounded shadow-2xl w-3/5">
+          <div className="flex-auto border rounded shadow-2xl w-4/6">
             <p className="ml-3 mt-3 font-bold">Các khóa học đã tham gia</p>
             <br />
             {/* section for one course begins */}
             <div className="flex px-2 ml-6 mb-7">
-              <img src={cover} alt="" className="rounded object-scale-down w-48" />
-              <div className="">
-                <p className='text-xl font-bold ml-8'>my COURSE???</p>
-                <p className="text-ellipsis overflow-hidden line-clamp-4 ml-8 mr-5">my desc???</p>
+              <div className="flex w-4/5">
+                <img src={cover} alt="" className="rounded object-scale-down w-48" />
+                <div className="">
+                  <p className='text-xl font-bold ml-8'>my COURSE???</p>
+                  <p className="text-ellipsis overflow-hidden line-clamp-4 ml-8 mr-5">my desc???</p>
+                </div>
+              </div>
+              <div className="w-1/5 flex flex-col">
+                <button
+                  type="button"
+                  className="m-auto text-black bg-orange-400 hover:bg-orange-800 border-2 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium w-28 py-1 text-center"
+                >
+                  Update
+                </button>
               </div>
             </div>
             <hr />
@@ -151,13 +161,23 @@ function YourProfile() {
             {/* section for one course ends */}
             {/* dummy data */}
             <div className="flex px-2 ml-6 mb-7">
-              <img src={cover} alt="" className="rounded object-scale-down w-48" />
-              <div className="">
-                <p className='text-xl font-bold ml-8'>my COURSE???</p>
-                <p className="text-ellipsis overflow-hidden line-clamp-4 ml-8 mr-5">
-                  Pizza Hut was launched on May 31, 1958, by two brothers, Dan and Frank Carney, both Wichita State
-                  students, as a single location in Wichita, Kansas. Six months later they opened a second outlet, and
-                  within a year they were operating six locations.</p>
+              <div className="flex w-4/5">
+                <img src={cover} alt="" className="rounded object-scale-down w-48" />
+                <div className="">
+                  <p className='text-xl font-bold ml-8'>my COURSE???</p>
+                  <p className="text-ellipsis overflow-hidden line-clamp-4 ml-8 mr-5">
+                    Pizza Hut was launched on May 31, 1958, by two brothers, Dan and Frank Carney, both Wichita State
+                    students, as a single location in Wichita, Kansas. Six months later they opened a second outlet, and
+                    within a year they were operating six locations.</p>
+                </div>
+              </div>
+              <div className="w-1/5 flex flex-col">
+                <button
+                  type="button"
+                  className="m-auto text-black bg-orange-400 hover:bg-orange-800 border-2 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium w-28 py-1 text-center"
+                >
+                  Update
+                </button>
               </div>
             </div>
             <hr />
